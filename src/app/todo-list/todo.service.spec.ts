48984c454d9083a642ba36d33496eec05c11777a
hojeongpark <hojeong.park@creatrip.com>
2022-01-30 02:07:02 +0900

    TodoService > 테스트 추가

--- src/app/todo-list/todo.service.spec.ts
+++ src/app/todo-list/todo.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { TodoService } from './todo.service';
+import {TodoService} from './todo.service';
 
 describe('TodoService', () => {
   let service: TodoService;
@@ -14,4 +14,40 @@ describe('TodoService', () => {
     expect(service).toBeTruthy();
   });
 
+  it('해야할 것이 없는 상태로 시작한다', function () {
+    // then
+    expect(service.getItems()).toHaveSize(0)
+  });
+
+  it('해야할 것을 추가할 수 있다', function () {
+    // given
+    const mockData = Object.freeze({
+      description: '밥먹기',
+    })
+
+    // when
+    service.addItem(mockData.description);
+
+    // then
+    expect(service.getItems()).toHaveSize(1)
+    expect(service.getItems()[0].description).toBe(mockData.description);
+  });
+
+  it('해야할 것을 여러 개 추가하면 마지막에 추가한게 제일 앞에 나온다', function () {
+    // given
+    const mockData = Object.freeze({
+      description1: '밥먹기',
+      description2: '양치하기',
+      description3: '잠자기',
+    })
+
+    // when
+    service.addItem(mockData.description1);
+    service.addItem(mockData.description2);
+    service.addItem(mockData.description3);
+
+    // then
+    expect(service.getItems()).toHaveSize(3)
+    expect(service.getItems()[0].description).toBe(mockData.description3);
+  });
 });
