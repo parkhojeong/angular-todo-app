a7d5f9ee2e43248f610f135593a4a23c801d097b
hojeongpark <hojeong.park@creatrip.com>
2022-01-29 22:18:32 +0900

    todo app > 단순 렌더링, 추가 기능

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TodoItem} from "./todo-item";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo-app';
+
+  allItems: TodoItem[] = [
+    {description: 'eat', done: true},
+    {description: 'sleep', done: false},
+  ];
+
+  get items() {
+    return this.allItems;
+  }
+
+  addItem(description: string) {
+    const newItem: TodoItem = {description, done: false};
+    this.allItems = [newItem, ...this.allItems];
+  }
 }
---
+++ src/app/todo-item.ts
@@ -0,0 +1,4 @@
+export type TodoItem = {
+  description: string;
+  done: boolean;
+}
