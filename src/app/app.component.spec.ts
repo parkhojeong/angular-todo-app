17c43f87f103547c60113ed70027983190e64ba3
hojeongpark <hojeong.park@creatrip.com>
2022-01-29 23:23:18 +0900

    todo app > 해야할 일 입력 테스트 추가

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {fireEvent, render, screen} from "@testing-library/angular";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -21,5 +22,21 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-todo-app');
   });
-
 });
+
+
+describe('todo list 테스트', () => {
+  it('해야할 아이템을 입력한 후 저장할 수 있다.', async () => {
+    // when
+    await render(AppComponent,)
+
+    // then
+    const input = screen.getByPlaceholderText('add an item')
+    fireEvent.change(input, {'target': {value: 'clean'}})
+
+    const addButton = screen.getByRole('button', {name: 'Add'})
+    fireEvent.click(addButton);
+
+    await screen.findByText('clean')
+  });
+})
