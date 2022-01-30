3e9ca36b21e7120a082bd7fee8f79884b3aec5b6
hojeongpark <hojeong.park@creatrip.com>
2022-01-30 22:17:40 +0900

    리팩토링 > todo service 디렉토리 변경

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {TodoService} from "./todo-list/todo.service";
+import {TodoService} from "./todo.service";
 
 @Component({
   selector: 'app-todo',
